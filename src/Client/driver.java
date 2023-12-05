d9665d056be7a078da7805fac1db2df0bad2766e
laixi <laixi@DESKTOP-E4F9HC0>
2023-12-05 23:15:44 +0800

    incomplete

--- src/Client/driver.java
+++ src/Client/driver.java
@@ -17,6 +17,252 @@ public class driver {
         // TODO code application logic here
         System.out.println("HIHI");
         System.out.println("Can use mah");
+        
+    /**
+     *
+     * Appointment Module Prepared By Lai Xiao Cherk (LXC)
+     *
+     *
+     */
+    public void treatmentMenu() {
+        int select = 0;
+
+        System.out.println("============================================");
+        System.out.println("\t\tMedical Service");
+        System.out.println("=============================================");
+        System.out.println("1.Display Treatment Service List");
+        System.out.println("2.Add the new Medical Service");
+        System.out.println("3.Update the Medical Service");
+        System.out.println("4.Remove the Medical Service");
+        System.out.println("5. Exit");
+
+        try {
+            System.out.println("\nPlease Choose a function:");
+            select = sc.nextInt();
+            System.out.println("\n");
+
+        } catch (Exception e) {
+            System.out.println("Invalid input!! Please insert the number(1-5 only) from the function selection above.");
+            System.out.println("Please click any key to continue");
+            System.out.println("\n");
+            treatmentMenu();
+        }
+
+        switch (select) {
+            case 1:
+                sc.nextLine();
+                displayTreatment();
+                System.out.println("Press enter back to menu");
+                System.out.println("\n");
+                sc.nextLine();
+                break;
+            case 2:
+                addTreatment();
+                break;
+            case 3:
+                updateTreatment();
+                break;
+            case 4:
+                removeTreatment();
+                break;
+            case 5:
+                System.out.println("Thanks You");
+                mainMenu();
+                break;
+        }
+        treatmentMenu();
+    }
+
+    public static void medicalHeader() {
+        System.out.println("===============================================================================");
+        System.out.println("|No.   |Medical Service Name      |Medicine             |Venue       |Price   |");
+        System.out.println("===============================================================================");
+    }
+
+    public void displayTreatment() {
+        medicalHeader();
+        for (int a = 1; a <= treatmentList.getNumberOfEntries(); a++) {
+            System.out.println(" " + a + ".     " + treatmentList.getEntry(a));
+        }
+
+//        TreatmentMenu();
+    }
+
+    public void addTreatment() {
+
+        String addTreatment = insertTreatment();
+        Medicine addMedicine = insertMedicine();
+        String addVenue = insertVenue();
+        double addPrice = insertPrice();
+        Treatment addTreatmentService = new Treatment(addTreatment, addMedicine, addVenue, addPrice);
+        treatmentList.add(addTreatmentService);
+        System.out.println("\n Treatment added successfully!\n");
+        treatmentMenu();
+    }
+
+    public String insertTreatment() {
+        String insertTreatment;
+        sc.nextLine();
+        do {
+
+            System.out.println("Insert Treatment service name:");
+            insertTreatment = sc.nextLine();
+
+        } while (!validation);
+        return insertTreatment;
+    }
+
+    public Medicine insertMedicine() {
+
+        Medicine[] allMedicine = new Medicine[medicineList.getNumberOfEntries()];
+        for (int a = 1; a <= allMedicine.length; a++) {
+            allMedicine[a - 1] = medicineList.getEntry(a);
+        }
+
+        int selection;
+
+        do {
+            validation = true;
+            for (int y = 0; y < allMedicine.length; y++) {
+                System.out.println(String.format("%d. %s", y + 1, allMedicine[y].getMedicine()));
+            }
+            System.out.println("Select a Medicine : ");
+            selection = sc.nextInt();
+            System.out.println("\n");
+
+            if (selection > allMedicine.length || selection <= 0) {
+                System.out.println("Please select a valid number");
+                validation = false;
+            }
+        } while (!validation);
+
+        return allMedicine[selection - 1];
+    }
+
+    public String insertVenue() {
+        String[] insertVenue = {"Room b123", "Room678", "Room345", "Room809", "Room165"};
+        int selection;
+
+        do {
+            for (int d = 0; d < insertVenue.length; d++) {
+                System.out.println(String.format("%d. %s", d + 1, insertVenue[d]));
+            }
+
+            System.out.println("Select a Venue: ");
+            selection = sc.nextInt();
+//            System.out.println("Selection: " + selection);
+            if (selection >= insertVenue.length || selection <= 0) {
+                System.out.println("Please select a valid number");
+                validation = false;
+            }
+
+        } while (!validation);
+        return insertVenue[selection - 1];
+    }
+
+    public double insertPrice() {
+        double insertPrice;
+        do {
+            System.out.println("Insert the treatment price: ");
+            insertPrice = sc.nextDouble();
+
+            if (insertPrice <= 0) {
+                validation = false;
+                System.out.println("Price cannot below zero, please enter again! ");
+            }
+        } while (!validation);
+        return insertPrice;
+    }
+
+    public void updateTreatment() {
+
+        String[] updateMenu = {"Treatment Name", "Medinice", "Venue", "Price", "Back"};
+        int choice;
+
+        //display update menu
+        System.out.println("\n");
+        displayTreatment();
+        System.out.println("\n Please select a treatment you want to modify: ");
+        choice = sc.nextInt();
+        if (choice > treatmentList.getNumberOfEntries()) {
+            System.out.println("Unable to modify, please try again!!\n");
+            updateTreatment();
+        }
+
+        //Save the modify treatment data into "modifyTreatment"
+        Treatment modifyTreatment = new Treatment();
+        modifyTreatment = treatmentList.getEntry(choice);
+        int modifyPosition = choice;
+
+        //Display the modify treatment value
+        medicalHeader();
+        System.out.println(" " + choice + ".     " + modifyTreatment);
+        System.out.println("\n");
+
+        // To display the menu
+        do {
+
+            for (int r = 0; r < updateMenu.length; r++) {
+                System.out.println(String.format("%2d.%s", r + 1, updateMenu[r]));
+            }
+            System.out.println("\n Please select a number you want to modify: ");
+            choice = sc.nextInt();
+
+        } while (choice > updateMenu.length || choice <= 0);
+
+        switch (choice) {
+            case 1:
+                modifyTreatment.setMedicalServices(insertTreatment());
+                break;
+            case 2:
+                modifyTreatment.setMedicine(insertMedicine());
+            case 3:
+                modifyTreatment.setVenue(insertVenue());
+                break;
+            case 4:
+                modifyTreatment.setPrice(insertPrice());
+                break;
+            case 5:
+                treatmentMenu();
+            default:
+                System.out.print("Please Enter A Valid Selection : ");
+                updateTreatment();
+                break;
+        }
+
+        treatmentList.replace(modifyPosition, modifyTreatment);
+
+        System.out.println("\n Updated Treatment Successfully!!\n");
+        treatmentMenu();
+
+    }
+
+    public void removeTreatment() {
+        medicalHeader();
+        for (int x = 1; x <= treatmentList.getNumberOfEntries(); x++) {
+            System.out.println(" " + x + ". " + treatmentList.getEntry(x));
+        }
+        System.out.println("Please select a treatment you want to remove: ");
+        int removeTreatment = sc.nextInt();
+        System.out.println(treatmentList.getEntry(removeTreatment));
+        System.out.println("Press y to delete the treatment: ");
+        char confirmation = sc.next().toUpperCase().charAt(0);
+
+        if (confirmation == 'Y') {
+
+            treatmentList.remove(removeTreatment);
+            displayTreatment();
+            System.out.println("\n");
+        } else {
+            displayTreatment();
+            System.out.println("\n");
+        }
+
+        treatmentMenu();
+    }
+
+}
+
     }
     
 }
