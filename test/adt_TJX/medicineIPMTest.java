80a71b0cf825e9ddabb7d26035e204e511bef1e4
Taruc <Taruc@B100K-PC9>
2023-12-18 11:25:58 +0800

    done 90%

--- test/adt_TJX/medicineIPMTest.java
+++ test/adt_TJX/medicineIPMTest.java
@@ -4,6 +4,7 @@
  */
 package adt_TJX;
 
+import Entity.Medicine;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,8 +17,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author User
  */
 public class medicineIPMTest {
+    private static final ListInterfaceTJX<Medicine> medicineList = new medicineIPM<>();
+    
     
     public medicineIPMTest() {
+        
     }
     
     @BeforeAll
@@ -36,110 +40,36 @@ public class medicineIPMTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of add method, of class medicineIPM.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        Object newEntry = null;
-        medicineIPM instance = new medicineIPM();
-        boolean expResult = false;
-        boolean result = instance.add(newEntry);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of remove method, of class medicineIPM.
-     */
-    @Test
-    public void testRemove() {
-        System.out.println("remove");
-        int givenPosition = 0;
-        medicineIPM instance = new medicineIPM();
-        Object expResult = null;
-        Object result = instance.remove(givenPosition);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
     /**
      * Test of replace method, of class medicineIPM.
      */
     @Test
     public void testReplace() {
+         int index = 5;
+       
+        Medicine[] m = new Medicine[index];
+        m[0] = new Medicine("Painkiller", 10.5, 10, "Pain relievers are medicines that reduce or relieve headaches,\n sore muscles, arthritis, or other aches and pains. \nThere are many different pain medicines, and each one has advantages and risks. \nSome types of pain respond better to certain medicines than others.\n");
+        m[1] = new Medicine("Antibiotics ", 11.5, 10, "Antibiotics are medicines that fight infections caused by bacteria\n in humans and animals by either killing the bacteria or \n making it difficult for the bacteria to grow and multiply. Bacteria are germs.");
+        m[2] = new Medicine("Antipyretics", 9.5, 10, "Antipyretics block or reverse fever's cytokine-mediated rise in\n core temperature, but do not affect body temperature in the afebrile state. \nThey are to be distinguished from hypothermia agents (cryogens),\n which are capable of lowering core temperature even in the absence of fever.\n");
+        m[3] = new Medicine("Vitamins", 12.5, 10, "Some vitamins help you resist infections and keep your nerves healthy,\n while others may help your body get energy from food or help your blood clot properly.");
+        m[4] = new Medicine("Morphine", 8, 10, "Morphine is used to relieve severe pain, such as pain caused by a major trauma or\n surgery, labour pain in childbirth or cancer pain.");
+
+        for (Medicine m1 : m) {
+            medicineList.add(m1);
+        }
+        
+        String medicine = "";
+        double price = 0.0;
+        int store = 1;
+        String detail = "";
+        
         System.out.println("replace");
         int givenPosition = 1;
-        Object newEntry = null;
-        medicineIPM instance = new medicineIPM();
-        instance.addEntry("Entry1");
-        instance.addEntry("Entry2");
-        instance.addEntry("Entry3");
         boolean expResult = false;
-        boolean result = instance.replace(givenPosition, newEntry);
+        boolean result = medicineList.replace(givenPosition, new Medicine(medicine, price, store, detail));
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of getEntry method, of class medicineIPM.
-     */
-    @Test
-    public void testGetEntry() {
-        System.out.println("getEntry");
-        int givenPosition = 0;
-        medicineIPM instance = new medicineIPM();
-        Object expResult = null;
-        Object result = instance.getEntry(givenPosition);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getNumberOfEntries method, of class medicineIPM.
-     */
-    @Test
-    public void testGetNumberOfEntries() {
-        System.out.println("getNumberOfEntries");
-        medicineIPM instance = new medicineIPM();
-        int expResult = 0;
-        int result = instance.getNumberOfEntries();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isEmpty method, of class medicineIPM.
-     */
-    @Test
-    public void testIsEmpty() {
-        System.out.println("isEmpty");
-        medicineIPM instance = new medicineIPM();
-        boolean expResult = false;
-        boolean result = instance.isEmpty();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of toString method, of class medicineIPM.
-     */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        medicineIPM instance = new medicineIPM();
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
