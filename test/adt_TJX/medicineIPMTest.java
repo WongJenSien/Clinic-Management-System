71726d4bddad920612c624ba4632d67eee1b9c43
User <User@LAPTOP-6LH8AK97.mshome.net>
2023-12-18 18:52:33 +0800

    Done 80%

--- test/adt_TJX/medicineIPMTest.java
+++ test/adt_TJX/medicineIPMTest.java
@@ -5,49 +5,26 @@
 package adt_TJX;
 
 import Entity.Medicine;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
 
 /**
  *
  * @author User
  */
 public class medicineIPMTest {
-    private static final ListInterfaceTJX<Medicine> medicineList = new medicineIPM<>();
+    private static final ListInterfaceTJX<Medicine> medicineList = new medicineIPM<>(); 
     
     
     public medicineIPMTest() {
-        
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
     }
 
-    
-    /**
-     * Test of replace method, of class medicineIPM.
-     */
-    @Test
-    public void testReplace() {
-         int index = 5;
-       
+    @BeforeEach
+    public void setUp() {
+        int index = 5;
+
         Medicine[] m = new Medicine[index];
         m[0] = new Medicine("Painkiller", 10.5, 10, "Pain relievers are medicines that reduce or relieve headaches,\n sore muscles, arthritis, or other aches and pains. \nThere are many different pain medicines, and each one has advantages and risks. \nSome types of pain respond better to certain medicines than others.\n");
         m[1] = new Medicine("Antibiotics ", 11.5, 10, "Antibiotics are medicines that fight infections caused by bacteria\n in humans and animals by either killing the bacteria or \n making it difficult for the bacteria to grow and multiply. Bacteria are germs.");
@@ -58,18 +35,79 @@ public class medicineIPMTest {
         for (Medicine m1 : m) {
             medicineList.add(m1);
         }
-        
-        String medicine = "";
-        double price = 0.0;
-        int store = 1;
-        String detail = "";
-        
-        System.out.println("replace");
-        int givenPosition = 1;
-        boolean expResult = false;
-        boolean result = medicineList.replace(givenPosition, new Medicine(medicine, price, store, detail));
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
+
+    /**
+     * Test of add method, of class medicineIPM.
+     */
+    @Test
+    public void testAdd() {
+        String newName = "New Medicine";
+        double newPrice = 15.0;
+        int newStore = 5;
+        String newDetail = "This is a new medicine for testing.";
+
+        // When
+        Medicine newMedicine = new Medicine(newName, newPrice, newStore, newDetail);
+        medicineList.add(newMedicine);
+
+        // Then
+        Medicine lastMedicine = medicineList.getEntry(medicineList.getNumberOfEntries());
+
+        assertEquals("Medicine name should match", newName, lastMedicine.getMedicine());
+        assertEquals("Medicine price should match", newPrice, lastMedicine.getPrice(), 0.001);
+        assertEquals("Medicine store should match", newStore, lastMedicine.getStore());
+        assertEquals("Medicine detail should match", newDetail, lastMedicine.getDetail());
+        fail("hihi");
+    }
+
+    /**
+     * Test of remove method, of class medicineIPM.
+     */
+    @Test
+    public void testRemove() {
+    }
+
+    /**
+     * Test of replace method, of class medicineIPM.
+     */
+    @Test
+    public void testReplace() {
+    }
+
+    /**
+     * Test of getEntry method, of class medicineIPM.
+     */
+    @Test
+    public void testGetEntry() {
+    }
+
+    /**
+     * Test of getNumberOfEntries method, of class medicineIPM.
+     */
+    @Test
+    public void testGetNumberOfEntries() {
+    }
+
+    /**
+     * Test of isEmpty method, of class medicineIPM.
+     */
+    @Test
+    public void testIsEmpty() {
+    }
+
+    /**
+     * Test of toString method, of class medicineIPM.
+     */
+    @Test
+    public void testToString() {
+    }
+
+    /**
+     * Test of addEntry method, of class medicineIPM.
+     */
+    @Test
+    public void testAddEntry() {
+    }
+    
 }
